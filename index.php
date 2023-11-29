aee7469f286c1f9bda5045b1ade13d545a322cad
Muhammad Fauzi Nur Aziz <axnvee18@gmail.com>
2023-11-29 22:48:54 +0700

    menambah login yang tidak jelas

--- index.php
+++ index.php
@@ -1,18 +1,37 @@
+<?php
+require('./app/Http/Conrtoller/Controller.php');
+$user = dataStore("SELECT * FROM users")[0];
+
+if(isset($_POST["login"])) {
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+
+  if ($user) {
+    if($password == $user["password"]) {
+      header("Location: http://localhost/web-rpl/resources/views/dashboard/");
+    } else {
+      header("Location: http://localhost/web-rpl/");
+    }
+  }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./src/css/login.css" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-    />
-    <title>Login</title>
-  </head>
-  <body>
-    <section>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./src/css/login.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
+  <title>Login</title>
+</head>
+
+<body>
+  <section>
+    <form action="" method="post">
       <div class="riyot">
         <div class="logo">
           <img src="./public/assets/image/logoPT.png" alt="" />
@@ -22,10 +41,10 @@
         </div>
         <div class="form">
           <div class="user">
-            <input type="text" placeholder="Username" />
+            <input type="email" name="email" id="email"  placeholder="Username" value=""/>
           </div>
           <div class="pas">
-            <input type="password" placeholder="Password" />
+            <input type="password" name="password" id="password" placeholder="Password" value=""/>
           </div>
         </div>
         <div class="remfor">
@@ -38,9 +57,11 @@
           </div>
         </div>
         <div class="conti">
-          <button>Login in now</button>
+          <button type="submit" name="login">Login in now</button>
         </div>
       </div>
-    </section>
-  </body>
+    </form>
+  </section>
+</body>
+
 </html>
