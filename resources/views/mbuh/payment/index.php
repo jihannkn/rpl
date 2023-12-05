bc77c220a1bf647f68296dfeeeee8760155b7a9c
jihannkn <khanzajihan@gmail.com>
2023-12-05 12:05:02 +0700

    nambah

---
+++ resources/views/mbuh/payment/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+        <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
+</head>
+<body>
+    <section>
+        <div class="payment">
+            <div class="left">
+                <img src="../../../../public/assets/image/balok.jpg" alt="">
+            </div>
+            <div class="right">
+                <h1>Batu Balok</h1>
+                <h3>Rp 450.000 /ton</h3>
+                <div class="jumlah">
+                    <button><i class="fa-solid fa-minus"></i></button>
+                    <span>1234</span>
+                    <button><i class="fa-solid fa-plus"></i></button>
+                </div>
+                <button>Beli    </button>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
